393708977efffe00620a6801a46a5212b3252dfd
RomaP28 <posokhovrom@gmail.com>
2023-10-12 21:34:04 +0300

    on Enter press focus search field

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import StarRating from "./StarRating";
 
 const average = (arr) =>
@@ -146,14 +146,32 @@ function Logo() {
 }
 
 function Search({query, setQuery}) {
-  return (
-      <input
-          className="search"
-          type="text"
-          placeholder="Search movies..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-      />
+    const inputEl = useRef(null);
+    
+    useEffect(()=>{
+        
+        function callback(e){
+            if(document.activeElement === inputEl.current) return;
+            if(e.code === 'Enter'){
+	            inputEl.current.focus();
+	            setQuery('');
+            }
+        }
+        
+        document.addEventListener('keydown', callback);
+        return () => document.removeEventListener('keydown', callback);
+        
+    },[setQuery]);
+    
+    return (
+        <input
+            ref={inputEl}
+            className="search"
+            type="text"
+            placeholder="Search movies..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+        />
   )
 }
 
